e2be5d2f9944abe730cc1e8d33e9ecc075b78165
HP <HP@PC>
2022-10-05 17:23:05 +0200

    Add project files.

---
+++ Industry Machine Park.Shared/Machine.cs
@@ -0,0 +1,10 @@
+﻿namespace Industry_Machine_Park.Shared
+{
+    public class Machine
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("");
+        public string Name { get; set; } = String.Empty;
+        public DateTime LastUpdated { get; set; } = default;
+        public MachineType? machinetype { get; set; } = default;
+    }
+}
---
+++ Industry Machine Park.Shared/MachineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Industry_Machine_Park.Shared
+{
+    public class MachineType
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
