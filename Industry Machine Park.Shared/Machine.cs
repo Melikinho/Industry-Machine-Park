ff694f4a6c07fe8cb07f7ef68fecf093aea5c8f2
HP <HP@PC>
2022-10-06 09:27:54 +0200

    Update properties

--- Industry Machine Park.Shared/Machine.cs
+++ Industry Machine Park.Shared/Machine.cs
@@ -4,7 +4,9 @@
     {
         public string Id { get; set; } = Guid.NewGuid().ToString("");
         public string Name { get; set; } = String.Empty;
+        public string Type { get; set; } = String.Empty;
         public DateTime LastUpdated { get; set; } = default;
-        public MachineType? machinetype { get; set; } = default;
+        public string Location { get; set; } = String.Empty;
+        public bool IsActive { get; set; } = default;
     }
 }
